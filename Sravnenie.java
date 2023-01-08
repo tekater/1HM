4ac6265e4ff58906f4276f155f5c601552a44e36
tekater <120014627+tekater@users.noreply.github.com>
2023-01-08 16:08:14 +0900

    Create Sravnenie.java

---
+++ Sravnenie.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            int size = scanner.nextInt();
+            int[] myArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+            int s = scanner.nextInt();
+            int sum = 1;
+		for (int a = 0; a < myArray.length; a++) {
+            if (myArray[a] > s) {
+                sum = sum + myArray[a];
+            }
+        }
+		System.out.println(sum); // у меня получается на 1 больше чем в примере, я не понимаю как фиксить.
+    }
+}
